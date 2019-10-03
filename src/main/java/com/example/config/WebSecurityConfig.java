49eb7ee15f4a9a36fcfe8a4604b2ea2d6a4c7b94
ramilssmusin <QAZwsx1@#*3579>
2019-10-03 05:23:44 -0400

    end of #2 with error on login page

--- src/main/java/com/example/config/WebSecurityConfig.java
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -33,8 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
-                        .username("U")
-                        .password("I")
+                        .username("u")
+                        .password("1")
                         .roles("USER")
                         .build();
 
