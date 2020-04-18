636155670adbf05e7956699545ce9f83aa0b412f
ramilssmusin <QAZwsx1@#*3579>
2020-04-18 16:08:05 -0400

    userService corrected #3

--- src/main/java/com/example/config/WebSecurityConfig.java
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -9,17 +9,19 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /*
     @Autowired
     private UserService userService;
+
+     */
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -46,9 +48,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                    .permitAll();
     }
 
+    /*
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
                 .passwordEncoder(passwordEncoder);
     }
+
+     */
 }
